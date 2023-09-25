1bf4f9adc39d2e272cf50df2035850848816c9d1
jluwoniu <jluwoniu@outlook.com>
2023-09-25 23:50:15 +0800

    修复偶尔找不到页面元素的问题

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,13 @@ use serde::{Deserialize, Serialize};
 use thirtyfour::prelude::*;
 use thirtyfour::{prelude::WebDriverResult, By, DesiredCapabilities, WebDriver};
 
-
 //const APP_NAME: &str = "epubit-integral";
 const CONFIG: &str = "./default-config.toml";
+//等待查找页面元素超时时间
+const TIMEOUT: Duration = Duration::from_secs(10);
+//等待查找点赞图标超时时间
+const DIANZAN_TIMEOUT: Duration = Duration::from_secs(3);
+const INTERVAL: Duration = Duration::from_millis(100);
 
 #[derive(Serialize, Deserialize, Default, Debug)]
 struct AppConfig {
@@ -38,15 +42,14 @@ pub fn add_account(
 
 pub async fn run() -> Result<(), Box<dyn Error>> {
     let mut cfg: AppConfig = confy::load_path(CONFIG)?;
-
-    // todo 通过命令行参数设置浏览器为edge
-
+    // todo 通过命令行参数设置浏览器为chrome
     for account in &mut cfg.accounts {
-        let caps = DesiredCapabilities::chrome();
+        let caps = DesiredCapabilities::edge();
         let driver = WebDriver::new("http://localhost:9515", caps).await?;
         login(&driver, account).await?;
         share_book(&driver, account).await?;
         share_course(&driver).await?;
+        sleep(Duration::from_secs(5));
         driver.quit().await?;
     }
     println!("{:?}", cfg);
@@ -56,41 +59,70 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
 
 async fn login(driver: &WebDriver, account: &Account) -> WebDriverResult<()> {
     driver.goto("https://www.epubit.com/").await?;
+    sleep(Duration::from_millis(300));
     let login_button = driver
-        .find(By::XPath("//*[@id='entry']/div[1]/nav/div[2]/div[1]/i[1]"))
+        .query(By::ClassName("login"))
+        .wait(Duration::from_secs(10), Duration::from_millis(100))
+        .first()
+        .await?
+        .query(By::Tag("i"))
+        .and_clickable()
+        .wait(TIMEOUT, INTERVAL)
+        .first()
         .await?;
     login_button.click().await?;
-    sleep(Duration::from_millis(2000));
-    let username_input = driver.find(By::Id("username")).await?;
-    sleep(Duration::from_millis(1000));
+    let username_input = driver
+        .query(By::Id("username"))
+        .wait(Duration::from_secs(10), Duration::from_millis(100))
+        .first()
+        .await?;
     username_input.send_keys(&account.username).await?;
-    let password_input = driver.find(By::Id("password")).await?;
-    sleep(Duration::from_millis(1000));
+    let password_input = driver
+        .query(By::Id("password"))
+        .wait(Duration::from_secs(10), Duration::from_millis(100))
+        .first()
+        .await?;
     password_input.send_keys(&account.password).await?;
-    let login_button = driver.find(By::Id("passwordLoginBtn")).await?;
+    let login_button = driver
+        .query(By::Id("passwordLoginBtn"))
+        .wait(Duration::from_secs(10), Duration::from_millis(100))
+        .first()
+        .await?;
     sleep(Duration::from_millis(500));
     login_button.click().await?;
     Ok(())
 }
 
+//点赞并分享图书
 async fn share_book(driver: &WebDriver, account: &mut Account) -> WebDriverResult<()> {
     driver.goto("https://www.epubit.com/books").await?;
     sleep(Duration::from_millis(300));
-
+    //点击下一页,直到上次运行保存的页数
     for _i in 1..account.page_number {
         driver
-            .find(By::ClassName("btn-next"))
+            .query(By::ClassName("btn-next"))
+            .and_clickable()
+            .wait(Duration::from_secs(10), Duration::from_millis(100))
+            .first()
             .await?
             .click()
             .await?;
-        sleep(Duration::from_millis(1500));
     }
-
+    //点赞图书的数量,每次点赞分享10本图书就够了
     let mut counter = 0;
-
     while counter < 10 {
-        let book_list_element = driver.find(By::ClassName("book-list")).await?;
-        let book_list = book_list_element.find_all(By::Tag("a")).await?;
+        sleep(Duration::from_millis(1000));
+        let book_list_element = driver
+            .query(By::ClassName("book-list"))
+            .wait(TIMEOUT, INTERVAL)
+            .first()
+            .await?;
+        let book_list = book_list_element
+            .query(By::Tag("a"))
+            .and_clickable()
+            .wait(TIMEOUT, INTERVAL)
+            .all()
+            .await?;
         for book in book_list {
             book.click().await?;
             for window in driver.windows().await? {
@@ -99,12 +131,17 @@ async fn share_book(driver: &WebDriver, account: &mut Account) -> WebDriverResul
                     driver.switch_to_window(window).await?;
                     if let Err(_) = driver
                         .query(By::ClassName("icon-dianzan"))
-                        .wait(Duration::from_millis(2500), Duration::from_millis(300))
+                        .wait(Duration::from_secs(3), Duration::from_millis(100))
                         .single()
                         .await
                     {
-                        let dianzan1 = driver.find(By::ClassName("icon-dianzan1")).await?;
-                        dianzan1.click().await?;
+                        //点赞图书
+                        driver
+                            .find(By::ClassName("icon-dianzan1"))
+                            .await?
+                            .click()
+                            .await?;
+                        //分享图书
                         driver
                             .find(By::ClassName("icon-2101fenxiang"))
                             .await?
@@ -112,7 +149,7 @@ async fn share_book(driver: &WebDriver, account: &mut Account) -> WebDriverResul
                             .await?;
                         counter += 1;
                         println!("点赞成功:{}", counter);
-                        sleep(Duration::from_millis(300));
+                        sleep(Duration::from_millis(1000));
                     }
                     driver.close_window().await?;
                     driver.switch_to_window(original_window).await?;
@@ -125,21 +162,28 @@ async fn share_book(driver: &WebDriver, account: &mut Account) -> WebDriverResul
             }
         }
         driver
-            .find(By::ClassName("btn-next"))
+            .query(By::ClassName("btn-next"))
+            .and_clickable()
+            .wait(TIMEOUT, INTERVAL)
+            .first()
             .await?
             .click()
             .await?;
-        sleep(Duration::from_millis(1500));
         account.page_number += 1;
     }
     account.page_number -= 1;
     Ok(())
 }
 
+//分享课程
 async fn share_course(driver: &WebDriver) -> WebDriverResult<()> {
     driver.goto("https://www.epubit.com/course").await?;
-    sleep(Duration::from_millis(500));
-    let course_list_element = driver.find(By::ClassName("course-list")).await?;
+    sleep(Duration::from_millis(300));
+    let course_list_element = driver
+        .query(By::ClassName("course-list"))
+        .wait(TIMEOUT, INTERVAL)
+        .first()
+        .await?;
     let course_list = course_list_element.find_all(By::Tag("a")).await?;
     for course in course_list {
         course.click().await?;
@@ -147,8 +191,12 @@ async fn share_course(driver: &WebDriver) -> WebDriverResult<()> {
             let original_window = driver.window().await?;
             if window != original_window {
                 driver.switch_to_window(window).await?;
-                let share_button = driver.find(By::ClassName("icon-2101fenxiang")).await?;
-                sleep(Duration::from_millis(500));
+                let share_button = driver
+                    .query(By::ClassName("icon-2101fenxiang"))
+                    .and_clickable()
+                    .wait(DIANZAN_TIMEOUT, INTERVAL)
+                    .first()
+                    .await?;
                 share_button.click().await?;
                 println!("分享课程成功");
                 sleep(Duration::from_millis(300));
